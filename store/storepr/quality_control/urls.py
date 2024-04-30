96489634426cc34dffbde6b96482b50cb3fa0d83
6i3ek3ua1 <slyantyaev20@yandex.ru>
2024-05-01 01:59:58 +0300

    задание 7

--- store/storepr/quality_control/urls.py
+++ store/storepr/quality_control/urls.py
@@ -4,7 +4,6 @@ from quality_control import views
 app_name = 'quality_control'
 
 
-
 urlpatterns = [
     path('', views.index, name='main'),
     path('bugs/', views.bug_report_list, name='bugs'),
@@ -12,5 +11,9 @@ urlpatterns = [
     path('bugs/<int:bug_id>/', views.BugDetailView.as_view(), name='bug_details'),
     path('features/<int:features_id>/', views.FeatureDetailView.as_view(), name='feature_detail'),
     path('bugs/new/', views.add_bug, name='create_bug'),
-    path('features/new/', views.add_feature, name='create_feature')
+    path('features/new/', views.add_feature, name='create_feature'),
+    path('bugs/<int:bug_id>/update/', views.update_bug, name='update_bug'),
+    path('features/<int:features_id>/update/', views.update_feature, name='update_feature'),
+    path('bugs/<int:bug_id>/delete/', views.delete_bug, name='delete_bug'),
+    path('features/<int:features_id>/delete/', views.delete_feature, name='delete_feature'),
 ]
